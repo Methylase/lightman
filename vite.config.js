2defcd9d0de684dc671e4fc2b22f70bc3c95568b
elevatesolutionet <elevatesolutionet2025@gmail.com>
2025-09-22 17:53:15 +0100

    update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  preview: {
+    allowedHosts: [
+      // Always allow localhost for dev
+      "localhost",
+      "127.0.0.1",
+      "lightmanng-gahdb6dsecayd4hq.centralus-01.azurewebsites.net",
+      // Pull host from env (e.g. when running on Azure)
+    ].filter(Boolean),
+  },
+});
